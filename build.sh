a11875c62eb16e12be9e9bfd7c2e3138e1a84935
Valentina <vh010200@gmail.com>
2023-03-27 17:56:04 +0200

    fixed makemigrations error

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ PGPASSWORD=bE5s7Jt6K7kIW6wTWB0hY9wRm4Bd0oiP psql -U pre_interview_task_user -h d
 PGPASSWORD=bE5s7Jt6K7kIW6wTWB0hY9wRm4Bd0oiP psql -U pre_interview_task_user -h dpg-cggr8264daddcg5h9nfg-a pre_interview_task -c 'CREATE DATABASE task_db'
 
 
-python manage.py makemigrations
+python manage.py makemigrations task
+python manage.py makemigrations tile
 python manage.py migrate
 python manage.py loaddata */fixtures/*.json
 python manage.py createsu
