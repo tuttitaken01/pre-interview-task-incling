3ab7d8c12de9f0860e8c00da70102c0cb2f4ae60
Valentina <vh010200@gmail.com>
2023-04-14 18:47:09 +0200

    fix 3

--- task/serializers.py
+++ task/serializers.py
@@ -11,7 +11,7 @@ class TaskSerializer(serializers.ModelSerializer):
     type = serializers.SlugRelatedField(
         many = False,
         slug_field='type',
-        queryset=Types.objects.values_list('id', flat=True)
+        queryset=Types.objects.values_list('type', flat=True)
     )
     class Meta:
         model = Tasks
