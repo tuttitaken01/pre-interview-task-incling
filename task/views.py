13bc8be130ff7ac99c208074618534c3faec8c0a
Valentina <vh010200@gmail.com>
2023-04-14 18:03:21 +0200

    view edit

--- task/views.py
+++ task/views.py
@@ -30,13 +30,6 @@ def task_detail(request, task_id=None):
             return Response(serializer.data)
 
     elif request.method == 'POST':
-        id = request.data.get('type')
-        try:
-            task_type = Types.object.get(type_id=id)
-        except Types.DoesNotExist:
-            return Response({'error': 'Invalid type id'}, status.status.HTTP_400_BAD_REQUEST)
-        request.data['type'] = task_type.type_id
-
         serializer = TaskSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
